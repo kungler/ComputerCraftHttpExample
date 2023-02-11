c7d09f995d608bedaf6ebfc3158402e649a26021
kungler <vincentgenty33@gmail.com>
2023-02-11 23:52:46 +0100

    antoine

--- website/website.js
+++ website/website.js
@@ -1,39 +1,52 @@
-const http = require('http');
-const ip = "172.17.0.0"
-const fs = require('fs');
-const port = 8080;
+// A webapp for taking http requests and replying with a JSON response
+// Create all functions for getting POST data, and sending JSON response
+// Create a server that listens on port 8080
+// Create a route for the server to listen on for POST requests
+// Create a route for the server to listen on for GET requests
+// Render the index.html file when a GET request is made to the server
 
-const requestHandler = (request, response) => {
+// server.js
+// where your node app starts
 
-  if (request.url.endsWith('.css')) {
-    fs.readFile(`.${request.url}`, (err, data) => {
-      if (err) {
-        response.writeHead(500, {'Content-Type': 'text/plain'});
-        response.end('Error loading stylesheet.css');
-      } else {
-        response.writeHead(200, {'Content-Type': 'text/css'});
-        response.end(data);
-      }
-    });
-  } else {
-    fs.readFile('./index.html', (err, data) => {
-      if (err) {
-        response.writeHead(500, {'Content-Type': 'text/plain'});
-        response.end('Error loading index.html');
-      } else {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(data);
-      }
-    });
-  }
-};
+// define process env port
+process.env.PORT = 56666;
 
-const server = http.createServer(requestHandler);
+// init project
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-server.listen(port,ip,(err) => {
-  if (err) {
-    return console.log('Error starting server: ', err);
-  }
+app.use(express.static('public'));
 
-  console.log(`Server is listening on ${port}`);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + 'index.html');
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// POST method route
+app.post('/', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  let chart = {
+    id:req.body.id,
+    firstname:req.body.firstname,
+}
+console.log(chart)
+
+  res.send('welcome, ' + req.body.name)
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
