24c9aea9c636ac9b8cca24cd66cc11afa4ecc0e9
kungler <vincentgenty33@gmail.com>
2023-02-11 21:31:58 +0100

    cbon

--- website/website.js
+++ website/website.js
@@ -29,7 +29,7 @@ const requestHandler = (request, response) => {
 
 const server = http.createServer(requestHandler);
 
-server.listen(port,(err) => {
+server.listen(port,"34.125.35.208",(err) => {
   if (err) {
     return console.log('Error starting server: ', err);
   }
