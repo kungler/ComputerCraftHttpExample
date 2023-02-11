f427c96c734443400095ad61bc8cc5fa12d0370d
kungler <vincentgenty33@gmail.com>
2023-02-12 00:40:06 +0100

    website

--- website/website.js
+++ website/website.js
@@ -16,7 +16,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+app.use(express.static('website'));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/index.html');
