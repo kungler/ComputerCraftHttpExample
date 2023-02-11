edcba97b90b3516446651bb0e54fb9bc28d778da
kungler <vincentgenty33@gmail.com>
2023-02-11 18:55:23 +0100

    V1.0.3

--- website/website.js
+++ website/website.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const port = 8080;
 
 const requestHandler = (request, response) => {
-
-if (request.url === '/index.html') {
   fs.readFile('./index.html', (err, data) => {
     if (err) {
       response.writeHead(500, {'Content-Type': 'text/plain'});
@@ -14,27 +12,26 @@ if (request.url === '/index.html') {
       response.end(data);
     }
   });
+};
 
-} else {
-
-fs.readFile('./stylesheet.css', (err, data) => {
+fs.readFile('./stylesheet.css', (err, cssData) => {
   if (err) {
-    console.error('Error loading style.css');
+    console.log('Error loading stylesheet.css');
   } else {
-    const css = data.toString();
-    response.writeHead(200, {'Content-Type': 'text/css'});
-    response.end(css);
+    const server = http.createServer((request, response) => {
+      if (request.url === '/stylesheet.css') {
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.end(cssData);
+      } else {
+        requestHandler(request, response);
+      }
+    });
+
+    server.listen(port, (err) => {
+      if (err) {
+        return console.log('Error starting server: ', err);
+      }
+      console.log(`Server is listening on ${port}`);
+    });
   }
-})
-
-}
-
-const server = http.createServer(requestHandler);
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('Error starting server: ', err);
-  }
-
-  console.log(`Server is listening on ${port}`);
-})};
+});
