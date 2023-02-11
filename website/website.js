a127fcf388655215284b4f1925e300298074e78e
kungler <vincentgenty33@gmail.com>
2023-02-11 18:58:04 +0100

    V1.0.31

--- website/website.js
+++ website/website.js
@@ -3,35 +3,35 @@ const fs = require('fs');
 const port = 8080;
 
 const requestHandler = (request, response) => {
-  fs.readFile('./index.html', (err, data) => {
-    if (err) {
-      response.writeHead(500, {'Content-Type': 'text/plain'});
-      response.end('Error loading index.html');
-    } else {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.end(data);
-    }
-  });
-};
-
-fs.readFile('./stylesheet.css', (err, cssData) => {
-  if (err) {
-    console.log('Error loading stylesheet.css');
-  } else {
-    const server = http.createServer((request, response) => {
-      if (request.url === '/stylesheet.css') {
-        response.writeHead(200, {'Content-Type': 'text/css'});
-        response.end(cssData);
+  if (request.url.endsWith('.css')) {
+    fs.readFile(`.${request.url}`, (err, data) => {
+      if (err) {
+        response.writeHead(500, {'Content-Type': 'text/plain'});
+        response.end('Error loading stylesheet.css');
       } else {
-        requestHandler(request, response);
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.end(data);
       }
     });
-
-    server.listen(port, (err) => {
+  } else {
+    fs.readFile('./index.html', (err, data) => {
       if (err) {
-        return console.log('Error starting server: ', err);
+        response.writeHead(500, {'Content-Type': 'text/plain'});
+        response.end('Error loading index.html');
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(data);
       }
-      console.log(`Server is listening on ${port}`);
     });
   }
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Error starting server: ', err);
+  }
+
+  console.log(`Server is listening on ${port}`);
 });
