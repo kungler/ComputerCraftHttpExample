debe5d9b1c67c1841a167fe23cde89f3300e3589
kungler <vincentgenty33@gmail.com>
2023-02-19 20:31:40 +0100

    test

--- minecraft/minecraft.js
+++ minecraft/minecraft.js
@@ -50,7 +50,9 @@ app.post('/', (req, res) => {
 
 function get_minecraft_response(json,tag)
 {
-  console.log(json)
+  console.log(json);
+  console.log(JSON.parse(json).Energy);
+  
   let json_list = toString(json).split(",");
 
   for (json_entity of json_list){
